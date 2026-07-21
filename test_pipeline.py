21b08b3852a105ceb4afec49bdd64ffb8b0033fd
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-21 14:56:05 +0600

    Update test_pipeline.py

--- test_pipeline.py
+++ test_pipeline.py
@@ -13,11 +13,6 @@ Exit code:
     0 = success
     1 = failure
 """
-- name: Debug package
-  run: |
-    pwd
-    ls -R
-    python -c "import sys; print(sys.path)"
 
 from pathlib import Path
 import sys
