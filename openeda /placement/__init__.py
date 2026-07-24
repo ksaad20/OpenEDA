51546308e4c17f03f04bb4011528ae9df4e12a4d
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-24 11:32:06 +0600

    Import Point and DevicePosition in __init__.py
    Added Point and DevicePosition to the module exports.

--- openeda /placement/__init__.py
+++ openeda /placement/__init__.py
@@ -6,6 +6,8 @@ and mixed-signal integrated circuit layouts.
 """
 
 from .placer import SimplePlacer
+from .placer import Point
+from .placer import DevicePosition
 
 __all__ = [
     "SimplePlacer",
